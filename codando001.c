6dc07e1e671e699c6aa60383034fe797a8443e16
eduardodematosferreira-cmyk <eduardodematosferreira@gmail.com>
2025-09-13 16:28:34 +0000

    versão 0.0.2

--- codando001.c
+++ codando001.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+int main(){
+    
+    int idade, matricula;
+    float altura;
+    char nome[50];
 
+    printf("Sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Sua altura: ");
+    scanf("%f", &altura);
+
+    printf("Seu nome: ");
+    scanf("%s", nome);
+
+    printf("Sua matricula: ");
+    scanf("%d", &matricula);
+
+    printf("\nNome do aluno: %s - Matrícula: %d\n", nome, matricula);
+    printf("Idade: %d - Altura: %.2f\n\n", idade, altura);
+    printf("");
+}
